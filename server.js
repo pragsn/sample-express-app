0ac64e0697782918c5891ac4c17b3cd9124c7393
pragsn <21pa1a04c6@vishnu.edu.in>
2023-08-19 02:41:15 -0700

    application

--- server.js
+++ server.js
@@ -1,1 +1,16 @@
-console.log("hi");
+const express = require('express')
+const app = express()
+const port = 3000
+
+app.get('/', (req, res) => {
+  res.send('Hello There! i am prajnabbhjfyugy')
+})
+
+
+app.get('/hey', (req, res) => {
+  res.send('sure!!')
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
